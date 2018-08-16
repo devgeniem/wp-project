cf555743d10d84ae7637ec2311d5f87dfeab75e8
Otto Rautamaa <otto.rautamaa@geniem.com>
2018-08-16 13:45:10 +0300

    Stateless constant change
    Change stateless cache busting constant to the new version

--- config/application.php
+++ config/application.php
@@ -193,7 +193,7 @@ define( 'WP_STATELESS_MEDIA_BODY_REWTITE', 'false' );
 define( 'WP_STATELESS_MEDIA_BODY_REWRITE', 'false' );
 define( 'WP_STATELESS_MEDIA_SERVICE_ACCOUNT ', env( 'GOOGLE_SERVICE_ACCOUNT_EMAIL' ) );
 define( 'WP_STATELESS_MEDIA_JSON_KEY', env( 'GOOGLE_CLOUD_STORAGE_ACCESS_KEY' ) );
-define( 'WP_STATELESS_MEDIA_HASH_FILENAME', 'true' );
+define( 'WP_STATELESS_MEDIA_CACHE_BUSTING', 'true' );
 
 // Replace the default bucket link and use the current domain. We serve uploads through a Nginx proxy cache.
 $scheme = defined( 'REQUEST_SCHEME' ) ? REQUEST_SCHEME : 'https';
