030b5a37fe8e4777e05e94a397ec58c50ffa1581
Juhani Hakanen <juhani.hakanen@geniem.com>
2019-10-30 12:53:42 +0200

    BASIC_AUTH_PASSWORD_HASH tuki

--- scripts/run-cron.sh
+++ scripts/run-cron.sh
@@ -3,6 +3,8 @@
 # If BASIC_AUTH_PASSWORD is set use basic authentication on curl.
 if [[ "${BASIC_AUTH_USER}" ]] && [[ "${BASIC_AUTH_PASSWORD}" ]]; then
 curl -u $BASIC_AUTH_USER:$BASIC_AUTH_PASSWORD $CRON_URL > /dev/null 2>&1
+elif [[ "${BASIC_AUTH_USER}" ]] && [[ "${BASIC_AUTH_PASSWORD_HASH}" ]]; then
+curl -u $BASIC_AUTH_USER:${BASIC_AUTH_PASSWORD_HASH/\{PLAIN\}/} $CRON_URL > /dev/null 2>&1
 else
 curl $CRON_URL > /dev/null 2>&1
 fi
