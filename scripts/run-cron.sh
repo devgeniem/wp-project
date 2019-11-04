d915736825233e1e8a78f958b33f86495194a76a
JuhaniGeniem <juhani.hakanen@geniem.com>
2019-11-04 15:25:22 +0200

    Windows to unix line-endings

--- scripts/run-cron.sh
+++ scripts/run-cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# If BASIC_AUTH_PASSWORD is set use basic authentication on curl.
+# If BASIC_AUTH_PASSWORD is set use basic authentication on curl..
 if [[ "${BASIC_AUTH_USER}" ]] && [[ "${BASIC_AUTH_PASSWORD}" ]]; then
 curl -u $BASIC_AUTH_USER:$BASIC_AUTH_PASSWORD $CRON_URL > /dev/null 2>&1
 elif [[ "${BASIC_AUTH_USER}" ]] && [[ "${BASIC_AUTH_PASSWORD_HASH}" ]]; then
