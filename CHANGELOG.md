9eaf9b9edad9288b7e1fea4db76a85f0e9e29b2f
Ville Siltala <ville.siltala@geniem.com>
2020-01-21 17:14:26 +0200

    Allow execution for run-cron.sh, allow insecure curl for run-cron

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Changed
+
+- Allow insecure connection when using curl for the cron url.
+- Change file permissions to allow execution for the run-cron.sh.
+
 ## [0.10.0] - 2019-11-19
 
 ### Changed
--- scripts/run-cron.sh
+++ scripts/run-cron.sh
@@ -5,9 +5,9 @@
 
 # If BASIC_AUTH_PASSWORD is set use basic authentication on curl.
 if [[ "${BASIC_AUTH_USER}" ]] && [[ "${BASIC_AUTH_PASSWORD}" ]]; then
-curl -u $BASIC_AUTH_USER:$BASIC_AUTH_PASSWORD $CRON_URL > /dev/null 2>&1
+curl -u $BASIC_AUTH_USER:$BASIC_AUTH_PASSWORD $CRON_URL --insecure > /dev/null 2>&1
 elif [[ "${BASIC_AUTH_USER}" ]] && [[ "${BASIC_AUTH_PASSWORD_HASH}" ]]; then
-curl -u $BASIC_AUTH_USER:${BASIC_AUTH_PASSWORD_HASH/\{PLAIN\}/} $CRON_URL > /dev/null 2>&1
+curl -u $BASIC_AUTH_USER:${BASIC_AUTH_PASSWORD_HASH/\{PLAIN\}/} $CRON_URL --insecure > /dev/null 2>&1
 else
-curl $CRON_URL > /dev/null 2>&1
+curl $CRON_URL --insecure > /dev/null 2>&1
 fi
